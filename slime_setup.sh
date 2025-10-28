dbcd128af47a372d1d7abc2990de9bdbf9b1f4a8
Zhuofeng-Li <89558990+Zhuofeng-Li@users.noreply.github.com>
2025-10-28 05:36:27 -0500

    Update slime_setup.sh

--- slime_setup.sh
+++ slime_setup.sh
@@ -1,1 +1,5 @@
+docker pull lizhuofeng/agentflow-slime:latest
 
+docker run --gpus all --ipc=host --shm-size=16g \
+  --ulimit memlock=-1 --ulimit stack=67108864 \
+  -it lizhuofeng/agentflow-slime:latest /bin/bash
