7c1c1c5a29c441fd8c5c32463083bf842ee3ec1f
Zhuofeng-Li <89558990+Zhuofeng-Li@users.noreply.github.com>
2025-12-13 06:05:20 -0600

    Update vastai_server_setup.sh

--- vastai_server_setup.sh
+++ vastai_server_setup.sh
@@ -19,3 +19,4 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install 18
 npm install -g @anthropic-ai/claude-code
+
