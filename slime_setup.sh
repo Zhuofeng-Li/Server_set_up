4cba3594e68895d503995110cf23bf577233c5a7
Zhuofeng-Li <89558990+Zhuofeng-Li@users.noreply.github.com>
2026-05-23 13:52:25 -0700

    Delete slime_setup.sh

--- slime_setup.sh
+++
@@ -1,5 +0,0 @@
-docker pull lizhuofeng/agentflow-slime:latest
-
-docker run --gpus all --ipc=host --shm-size=16g \
-  --ulimit memlock=-1 --ulimit stack=67108864 \
-  -it lizhuofeng/agentflow-slime:latest /bin/bash
